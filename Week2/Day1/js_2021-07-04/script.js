66bc5bb58aaf52876616f45a7dbbb386e0873e56
xer <xer@xer-labtop>
2021-07-04 13:36:28 +0300

    exercise xp

---
+++ Week2/Day1/js_2021-07-04/script.js
@@ -0,0 +1,16 @@
+// let addresNumer = 99;
+// let addresStreet = "Rothschild";
+// let country = "Israel";
+// let global_address = `${addresStreet} ${addresNumer}, ${country}`;
+// console.log(`I live in ${global_address}`);
+// let birthYear = 1996;
+// let futureYear = 2029;
+// let futureAge = futureYear - birthYear;
+// console.log(`I will be ${futureAge} in ${futureYear}`);
+
+// let a = ['cat','dog','fish','rabbit','cow'];
+// console.log(a[1]);
+// a.splice(-2, 1, 'horse');
+// console.log(a.length);
+
+let age = prompt('how old are you?', 20);
