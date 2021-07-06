e6c09419d084c039e16cf8d34328ef3ac683134c
xer <xer@xer-labtop>
2021-07-06 10:26:28 +0300

    daily challenge update: less lines

--- Week2/Day2/daily_challenge/script.js
+++ Week2/Day2/daily_challenge/script.js
@@ -1,8 +1,2 @@
-let sentence = 'The movie is not that bad, I like it';
-let wordNot = sentence.indexOf('not');
-let wordBad = sentence.indexOf('bad');
-if (wordBad > wordNot)
-{
-	sentence = sentence.replaceAll(/not.*bad/g, 'good');
-}
-console.log(sentence);
+let sentence = "The movie is not that bad, I like it";
+console.log(sentence.replaceAll(/not.*bad/g, "good"));
