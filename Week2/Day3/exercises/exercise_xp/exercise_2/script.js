808b2cdf6ceb4844be9cf267c5ec5f5936317124
xer <xer@xer-labtop>
2021-07-06 14:39:26 +0300

    exercise xp exercise_2

---
+++ Week2/Day3/exercises/exercise_xp/exercise_2/script.js
@@ -0,0 +1,23 @@
+let people = ["Greg", "Mary", "Devon", "James"];
+people.splice(people.indexOf('Greg'), 1);
+console.log(people);
+
+people.splice(people.indexOf('James'), 1, 'Jason');
+console.log(people);
+
+people.push('Eran');
+console.log(people);
+
+people = ["Greg", "Mary", "Devon", "James"];
+people.push('Eran');
+
+let cpy = [];
+while (people.length > 0) {
+	if (!(people[0] == 'Mary' || people[0] == 'Eran')) {
+		cpy = cpy.concat(people.splice(0,1));
+	}
+	else {
+		people.splice(0,1);
+	}
+}
+console.log(cpy);
