41a02ab0b7c38c89c19362eaabf3f6ea68bca2bc
xer <xer@xer-labtop>
2021-07-20 15:51:31 +0300

    daily challenge

--- Week4/Day3/daily_challenge/command.sh
+++ Week4/Day3/daily_challenge/command.sh
@@ -1,3 +1,7 @@
-if [ -z "$1" ]; then echo "usage: $0 <shift value>"; exit 1; fi
+#if [ -z "$1" ]; then echo "usage: $0 <shift value>"; exit 1; fi
 
-if [ -z "$(diff <(cat test.txt | python untitled.py encrypt $1 | python untitled.py decrypt $1) test.txt)" ]; then echo 'works'; else echo "doesn't work"; fi
+test () {
+	if [ -z "$(diff <(cat test.txt | python untitled.py encrypt $1 | python untitled.py decrypt $1) test.txt)" ]; then echo 'works'; else echo "doesn't work"; fi;
+}
+
+for i in {-200..200}; do echo $(test $i) with $i; done
