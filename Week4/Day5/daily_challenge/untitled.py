0bc8e91a0255906955833768327b7630100eae69
xer <xer@xer-labtop>
2021-07-22 16:10:44 +0300

    daily challenge: redo: implement my own alphabetical sorted function

--- Week4/Day5/daily_challenge/untitled.py
+++ Week4/Day5/daily_challenge/untitled.py
@@ -1,1 +1,32 @@
-print(','.join(sorted(input().split(','))).strip(','))
+def compare_str(a, b):
+    for l1, l2 in zip(a,b):
+        if l1 == l2:
+            continue
+        else:
+            return ord(l1) > ord(l2)
+    return len(a) > len(b) if not a == b else False
+
+
+def my_sorted2(words):
+    arr = words.copy()
+    for index, value in enumerate(arr[1:]):
+        for j, val in enumerate(arr[index:]):
+            if compare_str(arr[index + j - 1], arr[index + j]):
+                local = arr[index + j]
+                arr[j + index] = arr[j + index - 1]
+                arr[j + index - 1] = local
+    return arr
+
+def my_sorted(words):
+    arr = words.copy()
+    for _ in range(2):
+        arr = my_sorted2(arr)
+    return arr
+
+
+print(','.join(my_sorted(input().split(','))).strip(','))
+'''
+##same as this line from the previous git push of the daily challenge:
+#print(','.join(sorted(input().split(','))).strip(','))
+##but with an implementation of the sorted function.
+'''
