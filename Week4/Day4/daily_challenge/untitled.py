cf907cbbc7f4c399f34b03eddf92e73eb4a88e77
xer <xer@xer-labtop>
2021-07-22 09:55:35 +0300

    day 4's daily challenge with spacing

--- Week4/Day4/daily_challenge/untitled.py
+++ Week4/Day4/daily_challenge/untitled.py
@@ -1,5 +1,5 @@
 def decode_matrix(string):
-    return ''.join(filter(lambda x: x.isalpha(), [string.split('\n')[i][j]  for j in range(len(list(zip(*string.split('\n'))))) for i in range(len(string.split('\n')))]))
+    return ' '.join(filter(lambda x: None if len(x) == 0 else x,''.join(map(lambda x: x if x.isalpha() else ' ', [string.split('\n')[i][j]  for j in range(len(list(zip(*string.split('\n'))))) for i in range(len(string.split('\n')))])).split(' ')))
 
 print(decode_matrix('''7i3
 Tsi
