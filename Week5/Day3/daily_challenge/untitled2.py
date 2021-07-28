122cd31765f3213f0423c161af44000147ffca3b
xer <xer@xer-labtop>
2021-07-28 09:56:37 +0300

    Week5 Day3 daily challenge: fixed same name for method and property

--- Week5/Day3/daily_challenge/untitled2.py
+++ Week5/Day3/daily_challenge/untitled2.py
@@ -1,37 +1,37 @@
 class Circle():
     def __init__(self, **kwargs):
         if 'diameter' in kwargs:
-            self.diameter = kwargs['diameter']
+            self.d = kwargs['diameter']
             self.radius()
         elif 'radius' in kwargs:
-            self.radius = kwargs['radius']
+            self.r = kwargs['radius']
             self.diameter()
         else:
             self.radius = 1
     def diameter(self):
-        self.diameter = self.radius * 2
-        return self.diameter
+        self.d = self.r * 2
+        return self.d
     def radius(self):
-        self.radius = self.diameter / 2
-        return self.radius
+        self.r = self.d / 2
+        return self.r
     def area(self):
-        return 3.14159265 * (self.radius ** 2)
+        return 3.14159265 * (self.r ** 2)
     def __eq__(self, other):
-        return self.radius == other.radius
+        return self.r == other.r
     def __ge__(self, other):
-        return self.radius >= other.radius
+        return self.r >= other.r
     def __le__(self, other):
-        return self.radius <= other.radius
+        return self.r <= other.r
     def __ne__(self, other):
-        return self.radius != other.radius
+        return self.r != other.r
     def __lt__(self, other):
-        return self.radius < other.radius
+        return self.r < other.r
     def __gt__(self, other):
-        return self.radius > other.radius
+        return self.r > other.r
     def __add__(self, other):
-        return Circle(radius=self.radius + other.radius)
+        return Circle(radius=self.r + other.r)
     def __str__(self):
-        return 'circle:radius:' + str(self.radius)
+        return 'circle:radius:' + str(self.r)
 
 circle = Circle(radius=1)
 print(circle.area())
