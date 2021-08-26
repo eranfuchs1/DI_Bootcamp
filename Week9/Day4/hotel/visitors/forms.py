1caf47d0db5ec6a8bc81473caf7d6664c8c9d2f8
xer <xer@xer-labtop>
2021-08-26 12:13:30 +0300

    hotel project, writing the signup view, writing the signup form, can ommit the manual entry of email since its already in the auth usercreationform

--- Week9/Day4/hotel/visitors/forms.py
+++ Week9/Day4/hotel/visitors/forms.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class UserSignupForm(UserCreationForm):
-    email = forms.EmailField()
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name']
