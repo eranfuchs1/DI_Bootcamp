4b79153213c012c895e9aef8450086bec0641582
xer <xer@xer-labtop>
2021-08-26 11:53:19 +0300

    hotel project, writing the signup view, writing the actual view

--- Week9/Day4/hotel/visitors/views.py
+++ Week9/Day4/hotel/visitors/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .forms import UserSignupForm
+from django.views.generic.edit import CreateView
 
 # Create your views here.
+
+class SignupView(CreateView):
+    form_class = UserSignupForm
