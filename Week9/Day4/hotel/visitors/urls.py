86a8b59102c1fdb007d66f8d764f0f24a55d0cdd
xer <xer@xer-labtop>
2021-08-26 12:10:59 +0300

    hotel project, writing the signup view, writing the url route, fix the name (needs to be a keyword argument and not just positional)

--- Week9/Day4/hotel/visitors/urls.py
+++ Week9/Day4/hotel/visitors/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import SignupView
 
 urlpatterns = [
-    path('signup/', SignupView.as_view(), 'signup'),
+    path('signup/', SignupView.as_view(), name='signup'),
 ]
