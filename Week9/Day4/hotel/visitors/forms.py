86a83ca237a206dc2364146f7aced9db0effe7ce
xer <xer@xer-labtop>
2021-08-26 11:46:25 +0300

    hotel project, writing the signup view, writing the signup form

--- Week9/Day4/hotel/visitors/forms.py
+++ Week9/Day4/hotel/visitors/forms.py
@@ -4,3 +4,6 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserSignupForm(UserCreationForm):
     email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']
