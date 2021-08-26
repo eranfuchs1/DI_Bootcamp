23a29da6c6c7d445261417e60c70869c5b3d434c
xer <xer@xer-labtop>
2021-08-26 12:21:38 +0300

    hotel project, writing the core models, room field of Bookings is now a simple integer for simplification

--- Week9/Day4/hotel/visitors/models.py
+++ Week9/Day4/hotel/visitors/models.py
@@ -4,12 +4,8 @@ from django.db import models
 # Create your models here.
 
 
-class Rooms(models.Model):
-    room_number = models.IntegerField()
-
-
 class Bookings(models.Model):
     guest = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    room = models.ForeignKey(Rooms, on_delete=models.DO_NOTHING)
+    room = models.IntegerField()
     check_in_date = models.DateField(auto_now_add=True)
     check_out_date = models.DateField()
